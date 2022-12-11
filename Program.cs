bbc9db9888c3ee8e6d7fa2549549937a3fededf8
33684 <33684@DESKTOP-U3JQ29E>
2022-12-11 18:29:44 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module_exo_9bis
+{
+    class Program
+    { static void abs(int n,ref int result)
+        {
+            if(n < 0)
+            {
+                result = -n;
+            }
+            else
+            {
+                result = n;
+            }
+        }
+        static void Main(string[] args)
+        {// declaration 
+            int resultat = 0;
+            //saisie du nombre 
+            Console.Write("Entrez un nombre entier = ");
+            int nombre = int.Parse(Console.ReadLine());
+            //calcul de la valeur absolue 
+            abs(nombre, ref resultat);
+            //affichage de la valeur abolue 
+            Console.WriteLine("La valeur absolue de " + nombre + " est " + resultat);
+            Console.ReadLine();
+        }
+    }
+}
